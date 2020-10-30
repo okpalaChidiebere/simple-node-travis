8c08bf486f44f3c77eaa9806b00ebdcadf3f18ec
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-10-29 18:23:30 -0600

    Adding Environment Variables for code to use
    The docker build command has been modified to populate the environment variable. This tells Docker to take the environment variable that Travis had set as an environment variable and set it in the Docker image.

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+const favoriteFood = process.env.FAVORITE_FOOD;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
+    console.log(`My favorite food is ${favoriteFood}`);
     console.log('Containers rule!');
     await sleep(5000);
   }
